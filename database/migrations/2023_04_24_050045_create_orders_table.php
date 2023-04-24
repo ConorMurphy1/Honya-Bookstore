2889a9f7188698df2657c592405347e0cd183b21
ConorMurphy1 <conoromurichucmurphy@gmail.com>
2023-04-24 12:20:11 +0630

    conor

---
+++ database/migrations/2023_04_24_050045_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->int('customer_id');
+            $table->int('payment_type');
+            $table->int('card_no');
+            $table->int('total_amount');
+            $table->int('vat');
+            $table->int('grand_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
